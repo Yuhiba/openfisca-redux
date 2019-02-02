2a80c0515eeab52633578e5f83e3a58595e44f83
Laurent Bossavit <laurent.git@bossavit.com>
2019-02-02 23:30:27 +0100

    Allow for formula reuse while varying periods

--- tests/test_core.py
+++ tests/test_core.py
@@ -11,19 +11,32 @@ class Simulation:
 		self.functions = {}
 	def use(self, function, when = None):
 		self.functions[function.__name__] = (self.functions.get(function.__name__) or []) + [(function, when)]
-	def offset(self, parameter, period):
-		if parameter.annotation is not parameter.empty:
-			return eval(parameter.annotation, {"previous":previous}, {"period":period})
+	def period(self, parameter, period):
+		if parameter.annotation is "previous":
+			return previous(period)
 		else:
 			return period
+	def value(self, parameter, period):
+		if parameter.annotation is "previous":
+			return previous(period)
+		else:
+			return period
+	def evaluate(self, parameters, parameter, period, inputs):
+		p = parameters.get(parameter)
+		if p.annotation is "formula":
+			return self.formula(parameter, period)
+		else:
+			return self.calculate(parameter, self.period(p, period), inputs)
+	def formula(self, variable_name, period):
+		candidates = [function for (function, when) in self.functions[variable_name] if ((not when) or (when[0] <= period < when[1]))]
+		return candidates[0]
 	def calculate(self, variable_name, period, inputs):
 		if variable_name in inputs:
 			return inputs[variable_name][period]
 		if variable_name in self.functions:
-			candidates = [function for (function, when) in self.functions[variable_name] if ((not when) or (when[0] <= period < when[1]))]
-			func = candidates[0]
+			func = self.formula(variable_name, period)
 			parameters = inspect.signature(func).parameters
-			args = {parameter: self.calculate(parameter, self.offset(parameters.get(parameter), period), inputs) for parameter in parameters}
+			args = {parameter: self.evaluate(parameters, parameter, period, inputs) for parameter in parameters}
 			return func(**args)
 
 # OpenFisca's core mission is to calculate. If you give it
@@ -74,8 +87,67 @@ def test_calculate_by_period():
 def test_calculate_by_period():
 	simulation = Simulation()
 	inputs = {"rfr": {"2016": np.array([1000.0, 2000.0])}}
-	def allocation(rfr : "previous(period)"):
+	def allocation(rfr : "previous"):
 		return rfr * 0.01
 	simulation.use(allocation)
 	result = simulation.calculate("allocation", "2017", inputs)
 	assert result == approx(np.array([10.0, 20.0]))
+
+# We may want to reuse a formula's code, but vary its inputs
+
+def test_calculate_and_parametrize():
+	simulation = Simulation()
+	def salaire_net(salaire_brut, cotisations, csg_plafonnee):
+		return salaire_brut - cotisations - csg_plafonnee
+	def csg_plafonnee(salaire_brut):
+		return cotisations(np.maximum(salaire_brut, 1500)) * 0.1
+	def cotisations(salaire_brut):
+		return salaire_brut * 0.2
+	simulation.use(salaire_net)
+	simulation.use(csg_plafonnee)
+	simulation.use(cotisations)
+	inputs = {"salaire_brut": {"2016": np.array([1000.0, 2000.0])}}
+	result = simulation.calculate("salaire_net", "2016", inputs)
+	assert result == approx(np.array([770.0, 1560.0]))
+
+# However this will fail when this feature interacts with
+# the dated formula feature
+
+@mark.xfail
+def test_calculate_and_parametrize_failure():
+	simulation = Simulation()
+	def salaire_net(salaire_brut, cotisations, csg_plafonnee):
+		return salaire_brut - cotisations - csg_plafonnee
+	def csg_plafonnee(salaire_brut):
+		return cotisations(np.maximum(salaire_brut, 1500)) * 0.1
+	def cotisations(salaire_brut):
+		return salaire_brut * 0.2
+	simulation.use(cotisations, ("2016", "2017"))
+	def cotisations(salaire_brut):
+		return salaire_brut * 0.1
+	simulation.use(cotisations, ("2017", "2018"))
+	simulation.use(salaire_net)
+	simulation.use(csg_plafonnee)
+	inputs = {"salaire_brut": {"2016": np.array([1000.0, 2000.0])}}
+	result = simulation.calculate("salaire_net", "2016", inputs)
+	assert result == approx(np.array([770.0, 1560.0]))
+
+# Therefore, we provide a way of requesting the appropriate formula
+
+def test_calculate_and_parametrize_success():
+	simulation = Simulation()
+	def salaire_net(salaire_brut, cotisations, csg_plafonnee):
+		return salaire_brut - cotisations - csg_plafonnee
+	def csg_plafonnee(salaire_brut, cotisations : "formula"):
+		return cotisations(np.maximum(salaire_brut, 1500)) * 0.1
+	def cotisations(salaire_brut):
+		return salaire_brut * 0.2
+	simulation.use(cotisations, ("2016", "2017"))
+	def cotisations(salaire_brut):
+		return salaire_brut * 0.1
+	simulation.use(cotisations, ("2017", "2018"))
+	simulation.use(salaire_net)
+	simulation.use(csg_plafonnee)
+	inputs = {"salaire_brut": {"2016": np.array([1000.0, 2000.0])}}
+	result = simulation.calculate("salaire_net", "2016", inputs)
+	assert result == approx(np.array([770.0, 1560.0]))
